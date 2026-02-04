0cd04340830a6eeacc25e0c5968aae9a73ac3cbc
Danoobian <besslasz+github@gmail.com>
2026-02-04 15:14:39 +0100

    add functionality to convert from TextNode to HTMLNode with tests

---
+++ src/test_conversion.py
@@ -0,0 +1,38 @@
+import unittest
+
+from conversion import text_node_to_html_node
+from textnode import TextNode, TextType
+
+
+class TestConversion(unittest.TestCase):
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
+
+    def test_bold(self):
+        node = TextNode("This is a bold text node", TextType.BOLD)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "b")
+        self.assertEqual(html_node.value, "This is a bold text node")
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "a")
+        self.assertEqual(html_node.value, "This is a link")
+        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
+
+    def test_image(self):
+        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, "img")
+        self.assertEqual(html_node.value, "")
+        self.assertEqual(
+            html_node.props, {"src": "https://www.boot.dev", "alt": "This is an image"}
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()

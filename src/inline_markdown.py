ae4a8caf304e90810cb7cce9bd6393637a225136
Danoobian <besslasz+github@gmail.com>
2026-02-19 18:04:44 +0100

    reorganize functions and rename module

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -4,22 +4,14 @@ from htmlnode import LeafNode
 from textnode import TextNode, TextType
 
 
-def text_node_to_html_node(text_node):
-    if text_node.text_type not in TextType:
-        raise ValueError("wrong text type in conversion")
-    match text_node.text_type:
-        case TextType.TEXT:
-            return LeafNode(None, text_node.text)
-        case TextType.BOLD:
-            return LeafNode("b", text_node.text)
-        case TextType.ITALIC:
-            return LeafNode("i", text_node.text)
-        case TextType.CODE:
-            return LeafNode("code", text_node.text)
-        case TextType.LINK:
-            return LeafNode("a", text_node.text, {"href": text_node.url})
-        case TextType.IMAGE:
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+def text_to_textnodes(text):
+    new_nodes = [TextNode(text, TextType.TEXT)]
+    new_nodes = split_nodes_image(new_nodes)
+    new_nodes = split_nodes_link(new_nodes)
+    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
+    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
+    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
+    return new_nodes
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -93,13 +85,3 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
-
-
-def text_to_textnodes(text):
-    new_nodes = [TextNode(text, TextType.TEXT)]
-    new_nodes = split_nodes_image(new_nodes)
-    new_nodes = split_nodes_link(new_nodes)
-    new_nodes = split_nodes_delimiter(new_nodes, "`", TextType.CODE)
-    new_nodes = split_nodes_delimiter(new_nodes, "_", TextType.ITALIC)
-    new_nodes = split_nodes_delimiter(new_nodes, "**", TextType.BOLD)
-    return new_nodes
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,46 +1,17 @@
 import unittest
 
-from conversion import (
+from inline_markdown import (
     extract_markdown_images,
     extract_markdown_links,
     split_nodes_delimiter,
     split_nodes_image,
     split_nodes_link,
-    text_node_to_html_node,
     text_to_textnodes,
 )
 from textnode import TextNode, TextType
 
 
-class TestConversion(unittest.TestCase):
-    def test_text(self):
-        node = TextNode("This is a text node", TextType.TEXT)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, None)
-        self.assertEqual(html_node.value, "This is a text node")
-
-    def test_bold(self):
-        node = TextNode("This is a bold text node", TextType.BOLD)
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "b")
-        self.assertEqual(html_node.value, "This is a bold text node")
-
-    def test_link(self):
-        node = TextNode("This is a link", TextType.LINK, "https://www.boot.dev")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "a")
-        self.assertEqual(html_node.value, "This is a link")
-        self.assertEqual(html_node.props, {"href": "https://www.boot.dev"})
-
-    def test_image(self):
-        node = TextNode("This is an image", TextType.IMAGE, "https://www.boot.dev")
-        html_node = text_node_to_html_node(node)
-        self.assertEqual(html_node.tag, "img")
-        self.assertEqual(html_node.value, "")
-        self.assertEqual(
-            html_node.props, {"src": "https://www.boot.dev", "alt": "This is an image"}
-        )
-
+class TestInlineMarkdown(unittest.TestCase):
     def test_unbalanced_delimiter(self):
         node = TextNode("abc `def ghi", TextType.TEXT)
         with self.assertRaises(ValueError):

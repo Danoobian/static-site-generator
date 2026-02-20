c95d19f60ac580623ccd1c99a1d9582ff8aa56cb
Danoobian <besslasz+github@gmail.com>
2026-02-20 18:24:51 +0100

    add function and enum to process markdown blocks to block types

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,20 @@
+import re
+from enum import Enum
+
+
+class BlockType(Enum):
+    """
+    Defines Markdown block types.
+    """
+
+    PARAGRAPH = ("paragraph",)
+    HEADING = ("heading",)
+    CODE = ("code",)
+    QUOTE = ("quote",)
+    UNORDERED_LIST = ("unordered_list",)
+    ORDERED_LIST = "ordered_list"
+
+
 def markdown_to_blocks(markdown):
     """
     Convert markdown sting into a list of non-empty, stripped block of strings.
@@ -8,3 +25,21 @@ def markdown_to_blocks(markdown):
         for markdown_block in markdown_blocks
         if markdown_block.strip()
     ]
+
+
+def block_to_block_type(block):
+    """
+    Returns the type of the given Markdown block.
+    """
+    if re.match(r"#{1,6} ", block):
+        return BlockType.HEADING
+    elif re.match("^```[a-zA-Z0-9]*\n[\s\S]*\n```$", block):
+        return BlockType.CODE
+    elif all(re.match(r"^>\s*", line) for line in block.split("\n")):
+        return BlockType.QUOTE
+    elif all(re.match(r"^\s*[-+*]\s", line) for line in block.split("\n")):
+        return BlockType.UNORDERED_LIST
+    elif all(re.match(r"^\s*[1-9]\d*\.\s", line) for line in block.split("\n")):
+        return BlockType.ORDERED_LIST
+    else:
+        return BlockType.PARAGRAPH
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -1,6 +1,6 @@
 import unittest
 
-from block_markdown import markdown_to_blocks
+from block_markdown import BlockType, block_to_block_type, markdown_to_blocks
 
 
 class TestBlockMarkdown(unittest.TestCase):
@@ -30,3 +30,87 @@ This is the same paragraph on a new line
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_heading(self):
+        block = "### HEADING"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    def test_block_to_block_type_wrong_heading(self):
+        block = "######### HEADING"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_code(self):
+        block = """```python
+print("text")
+```"""
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
+    def test_block_to_block_type_wrong_code(self):
+        block = "```print('text')```"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_quote(self):
+        block = """> line1
+>line2
+> line3"""
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+
+    def test_block_to_block_type_wrong_quote(self):
+        block = """> line1
+line2
+> line3
+"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_wrong_indent_quote(self):
+        block = """> line1
+ > line2
+  > line3
+"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = """- element1
+ - element2
+  - element3"""
+        self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_wrong_unordered_list(self):
+        block = """- element1
+ element2
+  - element3"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_wrong_space_unordered_list(self):
+        block = """- element1
+-element2
+- element3"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = """1. first
+ 2. second
+  3. third"""
+        self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+
+    def test_block_to_block_type_0start_ordered_list(self):
+        block = """0. first
+ 2. second
+  3. third"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_wrong_ordered_list(self):
+        block = """1.first
+ 2. second
+  3.third"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_missing_ordered_list(self):
+        block = """1.first
+second
+  3.third"""
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_paragraph(self):
+        block = "test string"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
